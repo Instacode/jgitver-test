1445d79ea4ffc8ec1a13e8b70cb0b07806b561f6
Jason Edstrom <jason@instacode.io>
2020-01-21 17:40:26 -0600

    Add car unit test class.

---
+++ src/test/java/io/instacode/jgitver/CarTest.java
@@ -0,0 +1,21 @@
+package io.instacode.jgitver;
+
+import org.testng.annotations.Test;
+
+public class CarTest {
+
+  @Test
+  public void testGetMake() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  @Test
+  public void testGetModel() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  @Test
+  public void testGetMaxSpeed() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+}
